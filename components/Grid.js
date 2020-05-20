ca4e0baf8e7127ce14c36d39e5e7a9672efe9d85
Ruslan Dzyubanov <drumm2k@gmail.com>
2020-05-20 04:21:40 +0300

    Grid component with jsx styles Could use it in any project

--- components/Grid.js
+++ components/Grid.js
@@ -1,15 +1,54 @@
-import styled from 'styled-components';
-
-const GridLayer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgb(255 255 255 / 0.1);
-  pointer-events: none;
-`;
-
 export default function Grid() {
-  return <GridLayer />;
+  return (
+    <div className="grid">
+      <style jsx>{`
+        --grid-sides: 16px;
+        --grid-gutter: 16px;
+        --grid-columns: 4;
+        --grid-column-width: calc(
+          (100% - var(--grid-gutter) * var(--grid-columns)) /
+            var(--grid-columns)
+        );
+        --grid-color: rgb(0 255 0 / 0.1);
+
+        .grid {
+          position: fixed;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          pointer-events: none;
+
+          border: solid transparent;
+          border-width: 0 calc(var(--grid-sides) - var(--grid-gutter) / 2);
+
+          background-image: repeating-linear-gradient(
+            to right,
+            transparent 0,
+            transparent calc(var(--grid-gutter) / 2),
+            var(--grid-color) calc(var(--grid-gutter) / 2),
+            var(--grid-color)
+              calc(var(--grid-column-width) + var(--grid-gutter) / 2),
+            transparent calc(var(--grid-column-width) + var(--grid-gutter) / 2),
+            transparent calc(var(--grid-column-width) + var(--grid-gutter))
+          );
+          background-repeat: no-repeat;
+        }
+
+        @media (min-width: 736px) {
+          .grid {
+            --grid-sides: 24px;
+            --grid-columns: 8;
+          }
+        }
+
+        @media (min-width: 1024px) {
+          .grid {
+            --grid-sides: 32px;
+            --grid-columns: 12;
+          }
+        }
+      `}</style>
+    </div>
+  );
 }
